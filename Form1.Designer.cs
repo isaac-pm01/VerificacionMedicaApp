03dde4e25cdf0b273c70a0eec0c07cda3487c67f
Isaac Peña <isaacpm01@outlook.com>
2024-04-17 08:41:19 -0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace VerificacionMedicaApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            configuracionesToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem1 = new ToolStripMenuItem();
+            contextMenuStrip2 = new ContextMenuStrip(components);
+            anteriorToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip3 = new ContextMenuStrip(components);
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            contextMenuStrip4 = new ContextMenuStrip(components);
+            Menu = new ContextMenuStrip(components);
+            anteriorToolStripMenuItem1 = new ToolStripMenuItem();
+            contextMenuStrip5 = new ContextMenuStrip(components);
+            anteriorToolStripMenuItem2 = new ToolStripMenuItem();
+            conffiguaracionesToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem2 = new ToolStripMenuItem();
+            menuStrip1 = new MenuStrip();
+            anteriorToolStripMenuItem3 = new ToolStripMenuItem();
+            anteriorToolStripMenuItem4 = new ToolStripMenuItem();
+            configuracionesToolStripMenuItem1 = new ToolStripMenuItem();
+            salirToolStripMenuItem3 = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            contextMenuStrip1.SuspendLayout();
+            contextMenuStrip2.SuspendLayout();
+            Menu.SuspendLayout();
+            contextMenuStrip5.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 27);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(776, 209);
+            dataGridView1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(34, 265);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(272, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { salirToolStripMenuItem, configuracionesToolStripMenuItem, salirToolStripMenuItem1 });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(162, 70);
+            contextMenuStrip1.Opening += contextMenuStrip1_Opening;
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(161, 22);
+            salirToolStripMenuItem.Text = "Anterior";
+            // 
+            // configuracionesToolStripMenuItem
+            // 
+            configuracionesToolStripMenuItem.Name = "configuracionesToolStripMenuItem";
+            configuracionesToolStripMenuItem.Size = new Size(161, 22);
+            configuracionesToolStripMenuItem.Text = "Configuraciones";
+            // 
+            // salirToolStripMenuItem1
+            // 
+            salirToolStripMenuItem1.Name = "salirToolStripMenuItem1";
+            salirToolStripMenuItem1.Size = new Size(161, 22);
+            salirToolStripMenuItem1.Text = "Salir";
+            // 
+            // contextMenuStrip2
+            // 
+            contextMenuStrip2.Items.AddRange(new ToolStripItem[] { anteriorToolStripMenuItem });
+            contextMenuStrip2.Name = "contextMenuStrip2";
+            contextMenuStrip2.Size = new Size(118, 26);
+            // 
+            // anteriorToolStripMenuItem
+            // 
+            anteriorToolStripMenuItem.Name = "anteriorToolStripMenuItem";
+            anteriorToolStripMenuItem.Size = new Size(117, 22);
+            anteriorToolStripMenuItem.Text = "Anterior";
+            // 
+            // contextMenuStrip3
+            // 
+            contextMenuStrip3.Name = "contextMenuStrip3";
+            contextMenuStrip3.Size = new Size(61, 4);
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(312, 265);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Id";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(34, 306);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(272, 23);
+            textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(431, 265);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(271, 23);
+            textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(431, 306);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(271, 23);
+            textBox4.TabIndex = 9;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(312, 306);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 10;
+            label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(708, 265);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 15);
+            label3.TabIndex = 11;
+            label3.Text = "Correo";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(708, 306);
+            label4.Name = "label4";
+            label4.Size = new Size(52, 15);
+            label4.TabIndex = 12;
+            label4.Text = "Telefono";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(189, 368);
+            button1.Name = "button1";
+            button1.Size = new Size(435, 49);
+            button1.TabIndex = 13;
+            button1.Text = "Registrar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // contextMenuStrip4
+            // 
+            contextMenuStrip4.Name = "contextMenuStrip4";
+            contextMenuStrip4.Size = new Size(61, 4);
+            // 
+            // Menu
+            // 
+            Menu.Items.AddRange(new ToolStripItem[] { anteriorToolStripMenuItem1 });
+            Menu.Name = "Menu";
+            Menu.Size = new Size(118, 26);
+            // 
+            // anteriorToolStripMenuItem1
+            // 
+            anteriorToolStripMenuItem1.Name = "anteriorToolStripMenuItem1";
+            anteriorToolStripMenuItem1.Size = new Size(117, 22);
+            anteriorToolStripMenuItem1.Text = "Anterior";
+            // 
+            // contextMenuStrip5
+            // 
+            contextMenuStrip5.Items.AddRange(new ToolStripItem[] { anteriorToolStripMenuItem2, conffiguaracionesToolStripMenuItem, salirToolStripMenuItem2 });
+            contextMenuStrip5.Name = "contextMenuStrip5";
+            contextMenuStrip5.Size = new Size(172, 70);
+            contextMenuStrip5.Text = "Menu";
+            // 
+            // anteriorToolStripMenuItem2
+            // 
+            anteriorToolStripMenuItem2.Name = "anteriorToolStripMenuItem2";
+            anteriorToolStripMenuItem2.Size = new Size(171, 22);
+            anteriorToolStripMenuItem2.Text = "Anterior";
+            // 
+            // conffiguaracionesToolStripMenuItem
+            // 
+            conffiguaracionesToolStripMenuItem.Name = "conffiguaracionesToolStripMenuItem";
+            conffiguaracionesToolStripMenuItem.Size = new Size(171, 22);
+            conffiguaracionesToolStripMenuItem.Text = "Conffiguaraciones";
+            // 
+            // salirToolStripMenuItem2
+            // 
+            salirToolStripMenuItem2.Name = "salirToolStripMenuItem2";
+            salirToolStripMenuItem2.Size = new Size(171, 22);
+            salirToolStripMenuItem2.Text = "Salir";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { anteriorToolStripMenuItem3 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 18;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // anteriorToolStripMenuItem3
+            // 
+            anteriorToolStripMenuItem3.DropDownItems.AddRange(new ToolStripItem[] { anteriorToolStripMenuItem4, configuracionesToolStripMenuItem1, salirToolStripMenuItem3 });
+            anteriorToolStripMenuItem3.Name = "anteriorToolStripMenuItem3";
+            anteriorToolStripMenuItem3.Size = new Size(50, 20);
+            anteriorToolStripMenuItem3.Text = "Menu";
+            // 
+            // anteriorToolStripMenuItem4
+            // 
+            anteriorToolStripMenuItem4.Name = "anteriorToolStripMenuItem4";
+            anteriorToolStripMenuItem4.Size = new Size(180, 22);
+            anteriorToolStripMenuItem4.Text = "Anterior";
+            // 
+            // configuracionesToolStripMenuItem1
+            // 
+            configuracionesToolStripMenuItem1.Name = "configuracionesToolStripMenuItem1";
+            configuracionesToolStripMenuItem1.Size = new Size(180, 22);
+            configuracionesToolStripMenuItem1.Text = "Configuraciones";
+            // 
+            // salirToolStripMenuItem3
+            // 
+            salirToolStripMenuItem3.Name = "salirToolStripMenuItem3";
+            salirToolStripMenuItem3.Size = new Size(180, 22);
+            salirToolStripMenuItem3.Text = "Salir";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            contextMenuStrip1.ResumeLayout(false);
+            contextMenuStrip2.ResumeLayout(false);
+            Menu.ResumeLayout(false);
+            contextMenuStrip5.ResumeLayout(false);
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem configuracionesToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem1;
+        private ContextMenuStrip contextMenuStrip2;
+        private ToolStripMenuItem anteriorToolStripMenuItem;
+        private ContextMenuStrip contextMenuStrip3;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private ContextMenuStrip contextMenuStrip4;
+        private ContextMenuStrip Menu;
+        private ToolStripMenuItem anteriorToolStripMenuItem1;
+        private ContextMenuStrip contextMenuStrip5;
+        private ToolStripMenuItem anteriorToolStripMenuItem2;
+        private ToolStripMenuItem conffiguaracionesToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem anteriorToolStripMenuItem3;
+        private ToolStripMenuItem anteriorToolStripMenuItem4;
+        private ToolStripMenuItem configuracionesToolStripMenuItem1;
+        private ToolStripMenuItem salirToolStripMenuItem3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace VerificacionMedicaApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
